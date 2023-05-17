684d6138f454b2339cae8a919f2ae7ff02c04542
Juris Zemitis <juris.zemitis@gmail.com>
2023-05-17 10:59:36 +0300

    Day 2

--- dist/index.1ce41590.js
+++ dist/index.1ce41590.js
@@ -557,7 +557,81 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"gF2bz":[function(require,module,exports) {
-console.log("hi there");
+var _user = require("./models/User");
+const user = new (0, _user.User)({
+    name: "myname",
+    age: 20
+});
+user.on("change", ()=>{
+    console.log("Change #1");
+});
+user.on("change", ()=>{
+    console.log("Change #2");
+});
+user.on("save", ()=>{
+    console.log("Save triggered");
+});
+user.trigger("change");
+user.trigger("sa ve"); // "start:parcel": "npx parcel index.html"
+
+},{"./models/User":"96nTw"}],"96nTw":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "User", ()=>User);
+class User {
+    constructor(data){
+        this.data = data;
+        this.events = {};
+    }
+    get(propName) {
+        return this.data[propName];
+    }
+    set(update) {
+        Object.assign(this.data, update);
+    }
+    on(eventName, callback) {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+    trigger(eventName) {
+        const handlers = this.events[eventName];
+        if (!handlers || handlers.length === 0) return;
+        handlers.forEach((callback)=>{
+            callback();
+        });
+    }
+}
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"hZ54O"}],"hZ54O":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || dest.hasOwnProperty(key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
 
 },{}]},["aJ5jv","gF2bz"], "gF2bz", "parcelRequire94c2")
 
